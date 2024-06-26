6acfef1deae5733bb4d4bfe681fb28dc7b870830
Frederic Le Blay <fleblay@scaleway.com>
2024-06-26 11:01:07 +0200

    feat(harpoon)

---
+++ lua/custom/plugins/harpoon.lua
@@ -0,0 +1,39 @@
+return {
+  {
+    'ThePrimeagen/harpoon',
+    branch = 'harpoon2',
+    dependencies = { 'nvim-lua/plenary.nvim' },
+    config = function()
+      local harpoon = require 'harpoon'
+      harpoon:setup {}
+
+      vim.keymap.set('n', '<leader>a', function()
+        harpoon:list():add()
+      end, { desc = 'Add current buffer to Harpoon' })
+      vim.keymap.set('n', '<leader>1', function()
+        harpoon:list():select(1)
+      end, { desc = 'Jump to Harpoon 1' })
+      vim.keymap.set('n', '<leader>2', function()
+        harpoon:list():select(2)
+      end, { desc = 'Jump to Harpoon 2' })
+      vim.keymap.set('n', '<leader>2', function()
+        harpoon:list():select(2)
+      end, { desc = 'Jump to Harpoon 2' })
+      vim.keymap.set('n', '<leader>3', function()
+        harpoon:list():select(3)
+      end, { desc = 'Jump to Harpoon 3' })
+      vim.keymap.set('n', '<leader>4', function()
+        harpoon:list():select(4)
+      end, { desc = 'Jump to Harpoon 4' })
+      vim.keymap.set('n', '<leader>p', function()
+        harpoon:list():prev()
+      end, { desc = 'Jump to next Harpoon buffer' })
+      vim.keymap.set('n', '<leader>n', function()
+        harpoon:list():next()
+      end, { 'Jump to previous Harpoon buffer' })
+      vim.keymap.set('n', '<leader>e', function()
+        harpoon.ui:toggle_quick_menu(harpoon:list())
+      end, { desc = 'Open Harpoon window' })
+    end,
+  },
+}
